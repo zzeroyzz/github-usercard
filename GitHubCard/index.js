b40cec574f94c74ae466b2b03bf60a514f9d2d13
kato hawkins <kristhahawkins@icloud.com>
2020-07-17 21:33:20 -0400

    still working on MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
@@ -28,7 +29,6 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -58,3 +58,89 @@ const followersArray = [];
     luishrd
     bigknell
 */
+
+const cardCreator = (user) =>{
+
+  const card =document.createElement('div')
+  const userImg =document.createElement('img')
+  const userInfo = document.createElement('div')
+  const userH3  =document.createElement('h3')
+  const userName =document.createElement('p')
+  const userLocation = document.createElement('p')
+  const userProfile = document.createElement('p')
+  const userGit = document.createElement('a')
+  const followers = document.createElement('p')
+  const following =document.createElement('p')
+  const bio = document.createElement('p')
+
+  card.appendChild(userImg)
+  card.appendChild(userInfo)
+  userInfo.appendChild(userH3)
+  userInfo.appendChild(userName)
+  userInfo.appendChild(userLocation)
+  userInfo.appendChild(userProfile)
+  userProfile.appendChild(userGit)
+  userInfo.appendChild(followers)
+  userInfo.appendChild(following)
+  userInfo.appendChild(bio)
+
+
+  card.className.add ='card'
+  userInfo.className = 'card-info'
+  userH3.className = 'name'
+  userName.className ='username'
+
+
+ 
+
+  img.src = user.data.avatar_url
+  userInfo.className = 'card-info'
+  userH3.className = user.data.className
+  userH3.textcontent = `${user.data.name}`
+  userName.className = user.data.login
+  userName.textContent = `${user.data.login}`
+  userLocation.textContent = `Location: ${user.data.userLocation}`
+  userProfile.textContent = `Profile:`
+  userGit.href = user.data.html_url
+  userGit.textContent =`${user.data.html_url}`
+  followers.textContent =`Followers: ${user.followers_url}`
+  following.textContent =`Followers: ${user.following_url}`
+  bio.textContent =`Bio: ${user.data.bio}`
+   
+  return card
+
+ }
+ const cards = document.querySelector('.cards')
+ const userApi =`https://api.github.com/users/zzeroyzz`
+ axios.get(userApi)
+ 
+ .then((response) =>{
+  cards.appendChild(cardCreator(response.data))
+  const followers = response.data.followers_url
+  axios.get(followers)
+  .then((response) => {
+    console.log(response.data)
+    response.data.forEach(follower =>{
+      cards.appendChild(cardCreator(follower))
+    })
+    axios.get(`https://api.github.com/users/${follower.login}`)
+    .then((res) => {
+        cards.appendChild(cardCreator(response.data))
+    })
+  })
+  })
+   .catch((errorResponse) => {
+    console.log('error!', errorResponse)
+  })
+  
+  const followersArray = ['tetondan','dustinmyers','justsml','luishrd','bigknell'];
+
+  followersArray.forEach(profile =>{
+    axios.get(`https://api.github.com/users/${profile}`)
+    .then((response) =>{
+      cards.appendChild(cardCreator(response.data))
+    })
+    .catch((error) =>{
+      console.log(error)
+    })
+  })
